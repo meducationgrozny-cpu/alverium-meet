ab8ee009f28ff5d3c39a10b66a3a7c2a010fe51b
root <root@compute-vm-8-16-150-ssd-1775986855714.ru-central1.internal>
2026-07-15 05:07:23 +0000

    chore: добавлена CSP политика для встраивания комнаты через iframe (frame-ancestors)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' http://178.154.219.197 https://edu.alverium.ru http://localhost:*;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", 
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
